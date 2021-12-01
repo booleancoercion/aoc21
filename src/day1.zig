a84b39f6a621c257ba940ee844e0330768b26550
bool <booleancoercion@gmail.com>
2021-12-02 01:55:10 +0200

    Finished day1

---
+++ src/day1.zig
@@ -0,0 +1,70 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const input = @embedFile("../inputs/day1.txt");
+
+pub fn run(alloc: *Allocator, stdout: anytype) !void {
+    _ = alloc;
+    try stdout.print("-- Day 1 --\n", .{});
+    try stdout.print("Part 1: {}\n", .{try part1()});
+    try stdout.print("Part 2: {}\n", .{try part2()});
+}
+
+fn part1() !i32 {
+    var last: ?i32 = null;
+    var increments: i32 = 0;
+    var lines = std.mem.tokenize(u8, input, "\n");
+
+    while (lines.next()) |line| {
+        const parsed = try std.fmt.parseInt(i32, line, 0);
+        defer last = parsed;
+
+        if (last == null) {
+            continue;
+        }
+
+        if (parsed > last.?) {
+            increments += 1;
+        }
+    }
+
+    return increments;
+}
+
+fn part2() !i32 {
+    var window: [3]i32 = .{undefined} ** 3;
+    _ = window;
+
+    var increments: i32 = 0;
+
+    var lines = std.mem.tokenize(u8, input, "\n");
+    for (window) |_| {
+        const parsed = try std.fmt.parseInt(i32, lines.next().?, 0);
+        advanceWindow(&window, parsed);
+    }
+    var last_sum: i32 = getSum(window);
+
+    while (lines.next()) |line| {
+        const parsed = try std.fmt.parseInt(i32, line, 0);
+        advanceWindow(&window, parsed);
+
+        const sum = getSum(window);
+        defer last_sum = sum;
+
+        if (last_sum < sum) {
+            increments += 1;
+        }
+    }
+
+    return increments;
+}
+
+fn advanceWindow(window: *[3]i32, newval: i32) void {
+    window.*[0] = window.*[1];
+    window.*[1] = window.*[2];
+    window.*[2] = newval;
+}
+
+fn getSum(window: [3]i32) i32 {
+    return window[0] + window[1] + window[2];
+}
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,25 @@
 const std = @import("std");
+const art = @embedFile("../art.txt");
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
-}
+const day1 = @import("day1.zig");
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = &arena.allocator;
+
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("{s}\n", .{art});
+
+    var args = try std.process.argsAlloc(alloc);
+    defer std.process.argsFree(alloc, args);
+
+    const num_opt: ?i32 = if (args.len <= 1) null else std.fmt.parseInt(i32, args[1], 0) catch null;
+    const num = num_opt orelse 0;
+
+    try switch (num) {
+        0, 1 => day1.run(alloc, stdout),
+
+        else => std.log.err("Invalid Day!\n", .{}),
+    };
 }
