1f8372dcebd9a8f0bfa91225c39a4b62cbd4077a
bool <booleancoercion@gmail.com>
2021-12-13 17:26:10 +0200

    Optimized day13 + removed memory leak

--- src/day13.zig
+++ src/day13.zig
@@ -1,19 +1,24 @@
 const std = @import("std");
 const helper = @import("helper.zig");
 const Allocator = std.mem.Allocator;
-const Grid = helper.Grid;
+const HashMap = std.AutoHashMap;
+const ArrayList = std.ArrayList;
 
 const input = @embedFile("../inputs/day13.txt");
 
 pub fn run(alloc: Allocator, stdout_: anytype) !void {
     var parsed = try Input.init(alloc, input);
+    defer parsed.deinit();
 
-    parsed.fold(parsed.rules[0]);
+    const num_points = @intCast(usize, parsed.count());
+    var point_cache = try ArrayList(Input.Point).initCapacity(alloc, num_points);
+    defer point_cache.deinit();
+
+    try parsed.fold(parsed.rules.items[0], &point_cache);
     const res1 = parsed.count();
 
-    for (parsed.rules) |rule, i| {
-        if (i == 0) continue;
-        parsed.fold(rule);
+    for (parsed.rules.items[1..]) |rule| {
+        try parsed.fold(rule, &point_cache);
     }
 
     if (stdout_) |stdout| {
@@ -23,13 +28,120 @@ pub fn run(alloc: Allocator, stdout_: anytype) !void {
     }
 }
 
+const Input = struct {
+    points: HashMap(Point, void),
+    rules: ArrayList(Rule),
+
+    const Self = @This();
+
+    pub fn init(alloc: Allocator, inp: []const u8) !Self {
+        var points = HashMap(Point, void).init(alloc);
+        var rules = ArrayList(Rule).init(alloc);
+
+        var lines = tokenize(u8, inp, "\r\n");
+        while (lines.next()) |line| {
+            const point = try Self.parsePoint(line);
+            try points.put(point, {});
+
+            if (lines.rest()[0] == 'f') break;
+        }
+
+        while (lines.next()) |line| {
+            const rule = try Rule.init(line);
+            try rules.append(rule);
+        }
+
+        return Self{ .points = points, .rules = rules };
+    }
+
+    pub fn fold(self: *Self, rule: Rule, point_cache: *ArrayList(Point)) !void {
+        point_cache.clearRetainingCapacity();
+        var point_iter = self.points.keyIterator();
+        while (point_iter.next()) |point| {
+            try point_cache.append(point.*);
+        }
+
+        for (point_cache.items) |point| {
+            try self.foldPoint(point, rule);
+        }
+    }
+
+    pub fn count(self: Self) HashMap(Point, void).Size {
+        return self.points.count();
+    }
+
+    pub fn printGrid(self: Self, stdout: anytype) !void {
+        var max = Point{ .x = 0, .y = 0 };
+        var point_iter = self.points.keyIterator();
+
+        while (point_iter.next()) |point| {
+            max.x = std.math.max(max.x, point.x + 1);
+            max.y = std.math.max(max.y, point.y + 1);
+        }
+
+        var j: usize = 0;
+        while (j < max.y) : (j += 1) {
+            var i: usize = 0;
+            while (i < max.x) : (i += 1) {
+                const point = Point{ .x = i, .y = j };
+                const ch: u8 = if (self.points.contains(point)) '#' else ' ';
+                try stdout.print("{c}", .{ch});
+            }
+            try stdout.print("\n", .{});
+        }
+    }
+
+    fn foldPoint(self: *Self, point_: Point, rule: Rule) !void {
+        var point = point_;
+        switch (rule) {
+            .fold_x => |num| {
+                if (point.x < num) return;
+                _ = self.points.remove(point);
+                if (point.x > num) {
+                    const offset = point.x - num;
+                    point.x = num - offset;
+                    try self.points.put(point, {});
+                }
+            },
+            .fold_y => |num| {
+                if (point.y < num) return;
+                _ = self.points.remove(point);
+                if (point.y > num) {
+                    const offset = point.y - num;
+                    point.y = num - offset;
+                    try self.points.put(point, {});
+                }
+            },
+        }
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.points.deinit();
+        self.rules.deinit();
+    }
+
+    fn parsePoint(line: []const u8) !Point {
+        var splut = split(u8, line, ",");
+        const x_str = splut.next().?;
+        const y_str = splut.next().?;
+        if (splut.next() != null) unreachable;
+
+        const x = try parseUnsigned(usize, x_str, 10);
+        const y = try parseUnsigned(usize, y_str, 10);
+
+        return Point{ .x = x, .y = y };
+    }
+
+    const Point = struct { x: usize, y: usize };
+};
+
 const Rule = union(enum) {
     fold_x: usize,
     fold_y: usize,
 
     const Self = @This();
 
-    pub fn parseRule(line: []const u8) !Self {
+    pub fn init(line: []const u8) !Self {
         var tokens = tokenize(u8, line, " =");
         _ = tokens.next().?; // fold
         _ = tokens.next().?; // along
@@ -47,133 +159,6 @@ const Rule = union(enum) {
             unreachable;
         }
     }
-};
-
-const Input = struct {
-    grid: Grid(bool),
-    rules: []Rule,
-    allocator: Allocator,
-    limits: Point,
-
-    const Self = @This();
-
-    pub fn init(alloc: Allocator, str: []const u8) !Self {
-        var grid_limit: Point = Point{ .x = 0, .y = 0 };
-        var num_rules: usize = 0;
-
-        try Self.getLimits(str, &grid_limit, &num_rules);
-        var grid = try Grid(bool).init(alloc, grid_limit.x, grid_limit.y);
-        std.mem.set(bool, grid.data, false);
-        var rules = try alloc.alloc(Rule, num_rules);
-
-        var lines = tokenize(u8, str, "\r\n");
-        while (lines.next()) |line| {
-            const pt = try Self.parseLine(line);
-            grid.set(pt.x, pt.y, true);
-            if (lines.rest()[0] == 'f') break;
-        }
-
-        for (rules) |*rule| {
-            rule.* = try Rule.parseRule(lines.next().?);
-        }
-        if (lines.next() != null) unreachable;
-
-        return Self{
-            .grid = grid,
-            .rules = rules,
-            .allocator = alloc,
-            .limits = grid_limit,
-        };
-    }
-
-    pub fn count(self: Self) i32 {
-        var sum: i32 = 0;
-        var i: usize = 0;
-        while (i < self.limits.x) : (i += 1) {
-            var j: usize = 0;
-            while (j < self.limits.y) : (j += 1) {
-                if (self.grid.get(i, j)) sum += 1;
-            }
-        }
-
-        return sum;
-    }
-
-    pub fn fold(self: *Self, rule: Rule) void {
-        switch (rule) {
-            .fold_x => |num| {
-                var j: usize = 0;
-                while (j < self.limits.y) : (j += 1) {
-                    var i: usize = 1;
-                    while (i < self.limits.x - num) : (i += 1) {
-                        const val_original = self.grid.get(num - i, j);
-                        const val_overlay = self.grid.get(num + i, j);
-                        self.grid.set(num - i, j, val_original or val_overlay);
-                    }
-                }
-
-                self.limits.x = num;
-            },
-            .fold_y => |num| {
-                var i: usize = 0;
-                while (i < self.limits.x) : (i += 1) {
-                    var j: usize = 1;
-                    while (j < self.limits.y - num) : (j += 1) {
-                        const val_original = self.grid.get(i, num - j);
-                        const val_overlay = self.grid.get(i, num + j);
-                        self.grid.set(i, num - j, val_original or val_overlay);
-                    }
-                }
-
-                self.limits.y = num;
-            },
-        }
-    }
-
-    pub fn deinit(self: Self) void {
-        self.grid.deinit();
-        self.allocator.free(self.rules);
-    }
-
-    pub fn printGrid(self: Self, stdout: anytype) !void {
-        var j: usize = 0;
-        while (j < self.limits.y) : (j += 1) {
-            var i: usize = 0;
-            while (i < self.limits.x) : (i += 1) {
-                const ch: u8 = if (self.grid.get(i, j)) '#' else ' ';
-                try stdout.print("{c}", .{ch});
-            }
-            try stdout.print("\n", .{});
-        }
-    }
-
-    fn getLimits(str: []const u8, grid_limit: *Point, num_rules: *usize) !void {
-        var lines = tokenize(u8, str, "\r\n");
-        while (lines.next()) |line| {
-            const pt = try Self.parseLine(line);
-            grid_limit.x = std.math.max(pt.x + 1, grid_limit.x);
-            grid_limit.y = std.math.max(pt.y + 1, grid_limit.y);
-            if (lines.rest()[0] == 'f') break;
-        }
-
-        while (lines.next()) |_| {
-            num_rules.* += 1;
-        }
-    }
-
-    fn parseLine(line: []const u8) !Point {
-        var splut = split(u8, line, ",");
-        const x_str = splut.next().?;
-        const y_str = splut.next().?;
-        if (splut.next() != null) unreachable;
-
-        const x = try parseUnsigned(usize, x_str, 10);
-        const y = try parseUnsigned(usize, y_str, 10);
-
-        return Point{ .x = x, .y = y };
-    }
-
-    const Point = struct { x: usize, y: usize };
 };
 
 const eql = std.mem.eql;
